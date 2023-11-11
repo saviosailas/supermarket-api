c06c7a123b22098bf5a149afa3c98d5b4c5b7e12
savio <30656522+saviosailas@users.noreply.github.com>
2023-11-11 23:46:24 +0530

    updated urls

--- supermarket/urls.py
+++ supermarket/urls.py
@@ -6,4 +6,4 @@ from .routes import Login
 
 public_ns = Namespace(name="public", description="List of urls that doesn't require authentication", path="/api/v1")
 
-public_ns.add_resource(Login, "/", strict_slashes=0)
+public_ns.add_resource(Login, "/login", strict_slashes=0)
